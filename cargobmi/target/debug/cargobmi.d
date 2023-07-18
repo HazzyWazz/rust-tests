c3dd4f1993cbbe70cc48d428e7b1eff33e080185
HazzyWazz <valek.daniel@bottyan.eu>
2023-07-18 18:43:15 +0200

    cargo time

---
+++ cargobmi/target/debug/cargobmi.d
@@ -0,0 +1,1 @@
+C:\Users\Hazzy\Documents\Everything\Code\rust\ tests\cargobmi\target\debug\cargobmi.exe: C:\Users\Hazzy\Documents\Everything\Code\rust\ tests\cargobmi\src\main.rs
---
+++ cargobmi/target/debug/deps/cargobmi.d
@@ -0,0 +1,5 @@
+C:\Users\Hazzy\Documents\Everything\Code\rust tests\cargobmi\target\debug\deps\cargobmi.exe: src\main.rs
+
+C:\Users\Hazzy\Documents\Everything\Code\rust tests\cargobmi\target\debug\deps\cargobmi.d: src\main.rs
+
+src\main.rs:
