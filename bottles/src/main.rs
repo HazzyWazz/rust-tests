310dc480428a62080e9b7fcbb9f2f0c5a130f98b
HazzyWazz <valek.daniel@bottyan.eu>
2023-07-19 11:41:02 +0200

    Fix grammatical error and add 0

--- bottles/src/main.rs
+++ bottles/src/main.rs
@@ -3,7 +3,17 @@
 
 fn main() {
 	for num in (1..100).rev() {
-		println!("{num} bottles of beer on the wall, {num} bottles of beer,");
-		println!("Take one down, pass it around, {} bottles of beer on the wall", num-1)
+		if num != 1 {
+			println!("{num} bottles of beer on the wall, {num} bottles of beer,");
+			if num == 2 {
+				println!("Take one down, pass it around, {} bottle of beer on the wall", num-1);
+			} else {
+				println!("Take one down, pass it around, {} bottles of beer on the wall", num-1);
+			}
+		}
+		else {
+			println!("{num} bottle of beer on the wall, {num} bottle of beer,");
+			println!("Take one down, pass it around, no more bottles of beer on the wall");
+		}
 	}
 }
